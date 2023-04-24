af289dcf93441845ef70fc513c8ef11c6c214656
Jeff Whelpley <jeffwhelpley@gmail.com>
2023-04-24 19:21:20 -0400

    Trying to add google analytics in partytown

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -1,28 +1,31 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { QwikPartytown } from './components/partytown/partytown';
 import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
+    /**
+     * The root of a QwikCity site always start with the <QwikCityProvider> component,
+     * immediately followed by the document's <head> and <body>.
+     *
+     * Dont remove the `<head>` and `<body>` elements.
+     */
 
-  return (
-    <QwikCityProvider>
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
-      </body>
-    </QwikCityProvider>
-  );
+    return (
+        <QwikCityProvider>
+            <head>
+                <meta charSet="utf-8" />
+                <link rel="manifest" href="/manifest.json" />
+                <QwikPartytown forward={['dataLayer.push']} />
+                <script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-H137CJNKGP" />
+                <RouterHead />
+            </head>
+            <body lang="en">
+                <RouterOutlet />
+                <ServiceWorkerRegister />
+            </body>
+        </QwikCityProvider>
+    );
 });
