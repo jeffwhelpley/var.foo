e1fe05fc9925ce133cb7aedb2ab580d165e52d9e
Jeff Whelpley <jeffwhelpley@gmail.com>
2023-06-01 07:28:38 -0400

    Attempting to switch back to onRequest to see if that will work

--- frontend/functions/[[path]].ts
+++ frontend/functions/[[path]].ts
@@ -1,5 +1,3 @@
-// @ts-ignore
-
 // Cloudflare Pages Functions
 // https://developers.cloudflare.com/pages/platform/functions/
-export { fetch } from '../server/entry.cloudflare-pages';
+export { onRequest } from '../src/entry.cloudflare-pages';
